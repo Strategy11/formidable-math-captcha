99f9454bbc9079c4e9e191ed6df41198ea42b4e6
Steph <steph@strategy11.com>
2014-07-02 15:50:18 -0600

    cleanup auto-update fallbacks

--- FrmCptUpdate.php
+++ FrmCptUpdate.php
@@ -7,7 +7,7 @@ class FrmCptUpdate{
     var $pro_last_checked_store;
     var $version;
 
-    function FrmCptUpdate(){
+    function __construct(){
         if ( !class_exists('FrmUpdatesController') ) {
             return;
         }
@@ -19,19 +19,9 @@ class FrmCptUpdate{
         $this->pro_check_interval = 60*60*24; // Checking every 24 hours
 
         add_filter('site_transient_update_plugins', array( &$this, 'queue_update' ) );
-        
-        if ( method_exists('FrmAppHelper', 'plugin_version') ) {
-            $this->version = FrmAppHelper::plugin_version();
-        } else {
-            global $frm_version;
-            $this->version = $frm_version;
-        }
     }
 
     function queue_update($transient, $force=false){
-        if(version_compare($this->version, '1.07.0rc1', '<'))
-            return $transient;
-
         $plugin = $this;
         global $frm_update;
         if ( $frm_update ){
